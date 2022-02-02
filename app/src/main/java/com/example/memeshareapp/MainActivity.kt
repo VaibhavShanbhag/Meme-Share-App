9a2a49643ba6e71758dd7366497435eb7a2584eb
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-02-02 16:38:22 +0530

    Added volley and glide library

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.memeshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +22,25 @@ class MainActivity : AppCompatActivity() {
         nextbtn.setOnClickListener {
 
         }
+    }
+
+    private fun loadMeme(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://meme-api.herokuapp.com/gimme"
+
+        // Request a string response from the provided URL.
+        val jsonObjectRequest  = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                // Display the first 500 characters of the response string
+                val url = response.getString("url")
+                Glide.with(this)
+                    .load(url)
+                    .into(memeimage)
+            },
+            Response.ErrorListener { })
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
     }
 }
