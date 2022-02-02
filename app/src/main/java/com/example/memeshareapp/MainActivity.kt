6bac8d13fcef9b03e0aff246e2ed86f36010536c
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-02-02 17:58:05 +0530

    Added click listener to the next button

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.memeshareapp
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,16 +22,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadMeme()
+
         sharebtn.setOnClickListener {
 
         }
 
         nextbtn.setOnClickListener {
-
+            loadMeme()
         }
     }
 
     private fun loadMeme(){
+        progressbar.visibility = View.VISIBLE;
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "http://meme-api.herokuapp.com/gimme"
@@ -36,9 +46,32 @@ class MainActivity : AppCompatActivity() {
                 val url = response.getString("url")
                 Glide.with(this)
                     .load(url)
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ) : Boolean{
+                            progressbar.visibility = View.GONE
+                            return false;
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressbar.visibility = View.GONE
+                            return false
+                        }
+
+                    })
                     .into(memeimage)
             },
-            Response.ErrorListener { })
+            Response.ErrorListener {})
 
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest)
