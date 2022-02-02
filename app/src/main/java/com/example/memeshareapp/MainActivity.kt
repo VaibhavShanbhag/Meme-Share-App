dc43935c053a20d68ac15f260a585a65c4ceee31
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-02-02 18:32:17 +0530

    Added Singleton class for network call for api

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.memeshareapp
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -18,6 +18,8 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var currentImageURL: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,7 +27,11 @@ class MainActivity : AppCompatActivity() {
         loadMeme()
 
         sharebtn.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Hey Checkout this cool meme. I got from Reddit ${currentImageURL}")
+            val chooser = Intent.createChooser(intent,"Share this meme using..")
+            startActivity(chooser)
         }
 
         nextbtn.setOnClickListener {
@@ -35,17 +41,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadMeme(){
         progressbar.visibility = View.VISIBLE;
+        sharebtn.isEnabled = false
+        nextbtn.isEnabled = false
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
         val url = "http://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
         val jsonObjectRequest  = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
                 // Display the first 500 characters of the response string
-                val url = response.getString("url")
+                currentImageURL= response.getString("url")
                 Glide.with(this)
-                    .load(url)
+                    .load(currentImageURL)
                     .listener(object : RequestListener<Drawable>{
                         override fun onLoadFailed(
                             e: GlideException?,
@@ -54,6 +61,7 @@ class MainActivity : AppCompatActivity() {
                             isFirstResource: Boolean
                         ) : Boolean{
                             progressbar.visibility = View.GONE
+                            nextbtn.isEnabled = true
                             return false;
                         }
 
@@ -65,15 +73,20 @@ class MainActivity : AppCompatActivity() {
                             isFirstResource: Boolean
                         ): Boolean {
                             progressbar.visibility = View.GONE
+                            nextbtn.isEnabled = true
+                            sharebtn.isEnabled = true
                             return false
                         }
 
                     })
                     .into(memeimage)
             },
-            Response.ErrorListener {})
+            Response.ErrorListener {
+                Toast.makeText(this,"Something went wrong",Toast.LENGTH_SHORT).show()
+            })
 
         // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
     }
 }
